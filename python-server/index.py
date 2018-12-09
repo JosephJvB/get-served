f185e057c945a4cff725105bd7be41affd492fd9
JosephJvB <joevanbo@pm.me>
2018-12-09 21:04:13 +1300

    add comments to python server

--- python-server/index.py
+++ python-server/index.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
-api = Flask(__name__, template_folder="../static", static_folder="../static")
+api = Flask(
+  __name__, # ?
+  template_folder="../static", # serve html from here
+  static_folder="../static" # static assets are different to templates: js/css
+)
 
+# how to do a wildcard router?
 @api.route("/")
-def send_files():
+def send_files(): # method can have any name?
   api.send_static_file("index.js")
   return render_template("index.html")
 
